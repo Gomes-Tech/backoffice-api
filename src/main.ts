61a8c7e57b71673ef76223ccb957429b4c5a94a4
Marcos Gomes <marcosgomesdev@gmail.com>
2024-04-07 17:29:28 -0300

    feat: initial configuration docker, prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = app.get<ConfigService>(ConfigService);
+  const port = config.get<number>('PORT');
+
+  await app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
 }
 bootstrap();
