589a5385acd46480d191a7f98bc32de043c71a5c
Marcos Gomes <marcosgomesdev@gmail.com>
2025-05-29 00:50:24 -0300

    update main ts

--- src/main.ts
+++ src/main.ts
@@ -2,85 +2,22 @@ import { HttpExceptionFilter } from '@infra/filters';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
 import helmet from 'helmet';
-import { AppModule } from './app.module'; // Caminho ajustado com ./
-
-// Instância do Express para reutilização
-const server = express();
+import { AppModule } from './app.module';
 
 // Função para inicializar a aplicação
 async function bootstrap() {
-  try {
-    const app = await NestFactory.create(
-      AppModule,
-      new ExpressAdapter(server),
-      { logger: ['error', 'warn', 'log'] },
-    );
+  const app = await NestFactory.create(AppModule);
 
-    app.useGlobalPipes(new ValidationPipe());
-    app.useGlobalFilters(new HttpExceptionFilter());
-    app.enableCors({
-      origin: [
-        'https://backoffice-eta-seven.vercel.app/',
-        'http://localhost:3333',
-      ],
-      methods: 'GET,PATCH,POST,DELETE',
-      allowedHeaders: 'Content-Type, Authorization',
-      credentials: true,
-    });
-    app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors();
+  app.use(helmet());
 
-    // Configuração do Swagger apenas para ambiente de desenvolvimento
-    // if (process.env.NODE_ENV !== 'production') {
-    //   const config = new DocumentBuilder()
-    //     .setTitle('Documentação com Swagger - Rivera Admin API')
-    //     .setVersion('1.0')
-    //     .addTag('users')
-    //     .build();
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
 
-    //   const document = SwaggerModule.createDocument(app, config);
-    //   SwaggerModule.setup('api', app, document);
-    // }
-
-    await app.init();
-    return app;
-  } catch (error) {
-    console.error('Initialization error:', error);
-    throw error;
-  }
+  await app.listen(port, () => Logger.log(`http://localhost:${port}`));
 }
 
-let app: any;
-
-// Handler para ambiente serverless (Vercel)
-export default async function handler(req: any, res: any) {
-  try {
-    if (!app) {
-      app = await bootstrap();
-    }
-    server(req, res);
-  } catch (error) {
-    console.error('Request handler error:', error);
-    res
-      .status(500)
-      .send('Internal Server Error: ' + (error.message || 'Unknown error'));
-  }
-}
-
-// Inicialização para ambiente de desenvolvimento local
-if (process.env.NODE_ENV !== 'production') {
-  bootstrap()
-    .then((app) => {
-      const configService = app.get<ConfigService>(ConfigService);
-      const port = configService.get<number>('PORT') || 3333;
-
-      app.listen(port, () =>
-        Logger.log(`http://localhost:${port} - SERVER RUNNING`),
-      );
-    })
-    .catch((err) => {
-      Logger.error(`Error starting server: ${err}`);
-    });
-}
+bootstrap();
