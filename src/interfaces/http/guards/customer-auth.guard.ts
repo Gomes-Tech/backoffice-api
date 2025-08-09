700f3100c934b9d6f865fd6706eefa1ecb2eb1c2
Marcos Gomes <marcosgomesdev@gmail.com>
2025-08-09 10:11:55 -0300

    fix: corrections auth

--- src/interfaces/http/guards/customer-auth.guard.ts
+++ src/interfaces/http/guards/customer-auth.guard.ts
@@ -1,13 +1,26 @@
 import { UnauthorizedException } from '@infra/filters';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 
 @Injectable()
 export class CustomerAuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
 
