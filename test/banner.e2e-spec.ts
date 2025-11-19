2b89b0b0a7c8ddd04101e15464db1e1d1401ead3
Marcos Gomes <marcosgomesdev@gmail.com>
2025-11-19 12:16:34 -0300

    fix deploy

--- test/banner.e2e-spec.ts
+++ test/banner.e2e-spec.ts
@@ -1,8 +1,8 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from '../src/app.module';
 import { PrismaService } from '@infra/prisma';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import request from 'supertest';
+import { AppModule } from '../src/app.module';
 
 describe('Banner (e2e)', () => {
   let app: INestApplication;
